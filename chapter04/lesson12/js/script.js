2a985b2e49588e605ef685dfca2a35e75b7ae7de
kenterai <i-like-softbank.trikn@ezweb.ne.jp>
2022-05-05 17:01:39 +0900

    LESSON12

--- chapter04/lesson12/js/script.js
+++ chapter04/lesson12/js/script.js
@@ -1,3 +1,17 @@
 $(function(){
+  $("li").append("<div>");
 
+  $("div").each(function() {
+    $(this).html("<p>" + $(this).parent().children("img").attr("alt") + "</p>");
+  });
+
+  $("li").hover(function() {
+    $(this).children("div").stop().fadeIn(300);
+
+    $(this).children("div").children("p").stop().animate({"top" : 0}, 300);
+  }, function() {
+    $(this).children("div").stop().fadeOut(300);
+
+    $(this).children("div").children("p").stop().animate({"top" : "10px"}, 300);
+  });
 });
