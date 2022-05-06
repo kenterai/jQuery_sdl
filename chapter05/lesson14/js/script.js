cb31beb32294bc19b15669d03b2881a6f3b6ca03
kenterai <i-like-softbank.trikn@ezweb.ne.jp>
2022-05-06 14:11:09 +0900

    LESSON14

--- chapter05/lesson14/js/script.js
+++ chapter05/lesson14/js/script.js
@@ -1,3 +1,11 @@
 $(function(){
+  var h = 0;
 
+  $("main div").each(function() {
+    if($(this).height() > h) {
+      h = $(this).height();
+    }
+  });
+
+  $("main div").css("height", h + "px");
 });
