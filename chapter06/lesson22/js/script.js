a27ea234831903de73c60ad1af826c8f66653d05
kenterai <i-like-softbank.trikn@ezweb.ne.jp>
2022-05-08 11:34:44 +0900

    LESSON22

--- chapter06/lesson22/js/script.js
+++ chapter06/lesson22/js/script.js
@@ -1,3 +1,50 @@
 $(function() {
+  $(".alert").hide();
 
+  $("#submitBtn").click(function() {
+    var sendFlag = true;
+
+    if(!$("#text").val()) {
+      $("#textSection .alert").show();
+      sendFlag = false;
+    } else {
+      $("#textSection .alert").hide();
+    }
+
+    var radioChk = $("input[name = 'radio']:checked").length;
+
+    if(radioChk == 0) {
+      $("#radioSection .alert").show();
+      sendFlag = false;
+    } else {
+      $("#radioSection .alert").hide();
+    }
+
+    var chkboxChk = $("input[name = 'checkbox']:checked").length;
+
+    if(chkboxChk < 3) {
+      $("#checkboxSection .alert").show();
+      sendFlag = false;
+    } else {
+      $("#checkboxSection .alert").hide();
+    }
+
+    if($("select").val() == "none") {
+      $("#selectSection .alert").show();
+      sendFlag = false;
+    } else {
+      $("#selectSection .alert").hide();
+    }
+
+    if(!$("#textarea").val()) {
+      $("#textareaSection .alert").show();
+      sendFlag = false;
+    } else {
+      $("#textareaSection .alert").hide();
+    }
+
+    if(sendFlag == false) {
+      return false;
+    }
+  });
 });
