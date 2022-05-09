c91b315a26d316c04c686c6220274223cb1ef6e8
kenterai <i-like-softbank.trikn@ezweb.ne.jp>
2022-05-09 11:05:06 +0900

    LESSON25

--- chapter06/lesson25/js/script.js
+++ chapter06/lesson25/js/script.js
@@ -1,3 +1,11 @@
 $(function(){
+  var navPos = $("nav").offset().top;
 
+  $(window).scroll(function() {
+    if($(window).scrollTop() > navPos) {
+      $("nav").css("position", "fixed");
+    } else {
+      $("nav").css("position", "static");
+    }
+  });
 });
