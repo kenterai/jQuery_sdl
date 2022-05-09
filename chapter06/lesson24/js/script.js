ce82b2e9176c34c28bbc40166525960b896ea868
kenterai <i-like-softbank.trikn@ezweb.ne.jp>
2022-05-09 10:38:08 +0900

    LESSON24

--- chapter06/lesson24/js/script.js
+++ chapter06/lesson24/js/script.js
@@ -1,3 +1,9 @@
 $(function(){
-
+  $(window).scroll(function() {
+    if($(window).scrollTop() > 300) {
+      $("header").addClass("small");
+    } else {
+      $("header").removeClass("small");
+    }
+  });
 });
