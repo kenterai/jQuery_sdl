2c91a8d293078260609fa39a220d1b0aa1ac4841
kenterai <i-like-softbank.trikn@ezweb.ne.jp>
2022-05-11 08:05:20 +0900

    LESSON30

--- chapter07/lesson30/js/script.js
+++ chapter07/lesson30/js/script.js
@@ -1,3 +1,73 @@
 $(function() {
-  
-});
+  var countNum;
+
+  var cardArray;
+
+  var time;
+
+  var timer;
+
+  init();
+
+  function init() {
+    countNum = 1;
+    cardArray = [];
+    time = 0;
+
+    for(var i = 0; i <= 24; i++) {
+      cardArray.push(i);
+    }
+
+    for(var i = 0; i < cardArray.length; i++) {
+      var tmpNum = cardArray[i];
+      var r = Math.floor(Math.random()*cardArray.length);
+
+      cardArray[i] = cardArray[r];
+      cardArray[r] = tmpNum;
+    }
+
+    $("#numbers").html("");
+
+    for(var i = 0; i <= 24; i++) {
+      var cardNum = cardArray[i] + 1;
+      $("#numbers").prepend("<div>" + cardNum + "</div>");
+    }
+  }
+
+  $("button").click(function() {
+    $("#startScene").hide();
+
+    $("#numbers div").click(function() {
+      var num = $(this).html();
+
+      if(num == countNum) {
+        $(this).addClass("hit");
+
+        countNum++;
+
+        if(countNum == 26) {
+          clearInterval(timer);
+
+          $("#startScene p").html("Your Record : " + $("#timer span").html());
+
+          $("button").html("PLAY AGAIN");
+
+          $("#startScene").show();
+
+          if($("#record span").html() - $("#timer span").html() > 0 || $("#record span").html() == 0) {
+            $("#record span").html($("#timer span").html());
+          }
+          init();
+        }
+      }
+    });
+
+    timerFunc();
+    timer = setInterval(timerFunc, 10);
+  });
+
+  function timerFunc() {
+    time++;
+    $("#timer span").html(time);
+  }
+});s
